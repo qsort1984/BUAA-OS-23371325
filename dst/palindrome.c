cfb07f34c558d1533468bfdc969c6b628b013e84
吴思远 <23371325@buaa.edu.cn>
2025-03-06 20:17:13 +0800

    2nd

--- dst/palindrome.c
+++ dst/palindrome.c
@@ -3,13 +3,14 @@ int main() {
 	int n;
 	scanf("%d", &n);
 	
+	int t = n;
 	int tem = 0;
 	while (n!=0) {
 		tem = tem * 10 + n % 10;
 		n = n / 10; 
 	}
 
-	if (tem == n) {
+	if (tem == t) {
 		printf("Y\n");
 	} else {
 		printf("N\n");
