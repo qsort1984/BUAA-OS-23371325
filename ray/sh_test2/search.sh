1b1f246eeca6f5f831a8cd60bb8bd0a8735a8b39
吴思远 <23371325@buaa.edu.cn>
2025-03-06 20:29:30 +0800

    3rd

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+input=$1
+string=$2
+output=$3
+
+grep -n "$string" "$input" | cut -d: -f1 > "output"
