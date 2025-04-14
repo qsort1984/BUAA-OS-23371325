2930d097fb899cd6b48dca9931de199651aaf4f0
吴思远 <23371325@buaa.edu.cn>
2025-04-14 12:11:46 +0800

    pre 1st

--- kern/env.c
+++ kern/env.c
@@ -74,7 +74,8 @@ static void map_segment(Pde *pgdir, u_int asid, u_long pa, u_long va, u_int size
 		 *  Use 'pa2page' to get the 'struct Page *' of the physical address.
 		 */
 		/* Exercise 3.2: Your code here. */
-
+		struct Page *pp = pa2page(pa + i);
+		panic_on(page_insert(pgdir, asid, pp, va + i, perm));
 	}
 }
 
@@ -144,10 +145,16 @@ void env_init(void) {
 	/* Step 1: Initialize 'env_free_list' with 'LIST_INIT' and 'env_sched_list' with
 	 * 'TAILQ_INIT'. */
 	/* Exercise 3.1: Your code here. (1/2) */
+	LIST_INTT(&env_free_list);
+	TAILQ_INIT(&env_shed_list);
 
 	/* Step 2: Traverse the elements of 'envs' array, set their status to 'ENV_FREE' and insert
 	 * them into the 'env_free_list'. Make sure, after the insertion, the order of envs in the
 	 * list should be the same as they are in the 'envs' array. */
+	for (i = NENV - 1; i >= 0; i--) {
+			envs[i].env_status = ENV_FREE;
+			TAILQ_INSERT_HEAD(&env_free_list, &envs[i], env_link);
+	}
 
 	/* Exercise 3.1: Your code here. (2/2) */
 
@@ -184,6 +191,8 @@ static int env_setup_vm(struct Env *e) {
 	struct Page *p;
 	try(page_alloc(&p));
 	/* Exercise 3.3: Your code here. */
+	p->pp_ref++;
+	e->env_pgdir = (Pde *)page2kva(p);
 
 	/* Step 2: Copy the template page directory 'base_pgdir' to 'e->env_pgdir'. */
 	/* Hint:
@@ -224,9 +233,14 @@ int env_alloc(struct Env **new, u_int parent_id) {
 
 	/* Step 1: Get a free Env from 'env_free_list' */
 	/* Exercise 3.4: Your code here. (1/4) */
+	if (LIST_EMPTY(&env_free_list)) {
+		return -E_NO_FREE_ENV;
+	}
+	e = LIST_FIRST(&env_free_list);
 
 	/* Step 2: Call a 'env_setup_vm' to initialize the user address space for this new Env. */
 	/* Exercise 3.4: Your code here. (2/4) */
+	try(env_setup_vm(e));
 
 	/* Step 3: Initialize these fields for the new Env with appropriate values:
 	 *   'env_user_tlb_mod_entry' (lab4), 'env_runs' (lab6), 'env_id' (lab3), 'env_asid' (lab3),
@@ -238,6 +252,9 @@ int env_alloc(struct Env **new, u_int parent_id) {
 	 */
 	e->env_user_tlb_mod_entry = 0; // for lab4
 	e->env_runs = 0;	       // for lab6
+	e->env_id = mkenvid(e);
+	try(asid_alloc(&e->env_asid));
+	e->env_parent_id = parent_id;
 	/* Exercise 3.4: Your code here. (3/4) */
 
 	/* Step 4: Initialize the sp and 'cp0_status' in 'e->env_tf'.
@@ -251,6 +268,7 @@ int env_alloc(struct Env **new, u_int parent_id) {
 
 	/* Step 5: Remove the new Env from env_free_list. */
 	/* Exercise 3.4: Your code here. (4/4) */
+	LIST_REMOVE(e, env_link);
 
 	*new = e;
 	return 0;
@@ -281,13 +299,14 @@ static int load_icode_mapper(void *data, u_long va, size_t offset, u_int perm, c
 
 	/* Step 1: Allocate a page with 'page_alloc'. */
 	/* Exercise 3.5: Your code here. (1/2) */
+	try(page_alloc(&p));
 
 	/* Step 2: If 'src' is not NULL, copy the 'len' bytes started at 'src' into 'offset' at this
 	 * page. */
 	// Hint: You may want to use 'memcpy'.
 	if (src != NULL) {
 		/* Exercise 3.5: Your code here. (2/2) */
-
+		memcpy((char *)page2kva(p) + offset, src, len);
 	}
 
 	/* Step 3: Insert 'p' into 'env->env_pgdir' at 'va' with 'perm'. */
@@ -323,6 +342,7 @@ static void load_icode(struct Env *e, const void *binary, size_t size) {
 	/* Step 3: Set 'e->env_tf.cp0_epc' to 'ehdr->e_entry'. */
 	/* Exercise 3.6: Your code here. */
 
+	e->env_tf.cp0_epc = edhr->e_entry;
 }
 
 /* Overview:
