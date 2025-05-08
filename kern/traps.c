44c76e34b6f5546230305864b175583b84f8521e
吴思远 <23371325@buaa.edu.cn>
2025-05-08 08:25:23 +0800

    pre 1st

--- kern/traps.c
+++ kern/traps.c
@@ -8,11 +8,15 @@ extern void handle_tlb(void);
 extern void handle_sys(void);
 extern void handle_mod(void);
 extern void handle_reserved(void);
+extern void handle_adel(void);
+extern void handle_ades(void);
 
 void (*exception_handlers[32])(void) = {
     [0 ... 31] = handle_reserved,
     [0] = handle_int,
     [2 ... 3] = handle_tlb,
+	[4] = handle_adel,
+	[5] = handle_ades,
 #if !defined(LAB) || LAB >= 4
     [1] = handle_mod,
     [8] = handle_sys,
@@ -26,4 +30,13 @@ void (*exception_handlers[32])(void) = {
 void do_reserved(struct Trapframe *tf) {
 	print_tf(tf);
 	panic("Unknown ExcCode %2d", (tf->cp0_cause >> 2) & 0x1f);
+}
+
+void do_adel(struct Trapframe *tf) {
+ 	// 在此实现相应操作以使修改后指令符合要求
+	u_long epc = tf->cp0_epc;
+}
+
+void do_ades(struct Trapframe *tf) {
+ 	// 在此实现相应操作以使修改后指令符合要求
 }
