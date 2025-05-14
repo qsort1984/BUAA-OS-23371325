0f95e30ab060d2901d613581d990ee65f3b9dcb9
吴思远 <23371325@buaa.edu.cn>
2025-05-14 19:57:57 +0800

    1

--- kern/env.c
+++ kern/env.c
@@ -262,8 +262,8 @@ int env_alloc(struct Env **new, u_int parent_id) {
 	 */
 	e->env_user_tlb_mod_entry = 0; // for lab4
 	e->env_runs = 0;	       // for lab6
-	e->env_id = mkenvid(e);
 	try(asid_alloc(&e->env_asid));
+	e->env_id = mkenvid(e);
 	e->env_parent_id = parent_id;
 	/* Exercise 3.4: Your code here. (3/4) */
 
