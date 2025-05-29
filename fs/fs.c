e5500293fe5412e152adb984251078bc303930fc
吴思远 <23371325@buaa.edu.cn>
2025-05-29 19:51:30 +0800

    2nd

--- fs/fs.c
+++ fs/fs.c
@@ -24,7 +24,7 @@ int traverse_file(const char *path, struct File *file, const char *name, struct 
 	if (/*file 的名字等于 name*/strcmp(file->f_name, name) == 0) {
 		/*增加 res->count*/
 		/*添加 res 的路径*/
-		strcpy(path, res->file_path[res->count++]);
+		strcpy(res->file_path[res->count++], path);
 	}
 	if (file->f_type == FTYPE_DIR) {
 		for (int i = 0; i < nblock; i++) {
@@ -36,11 +36,12 @@ int traverse_file(const char *path, struct File *file, const char *name, struct 
 				char curpath[MAXPATHLEN + MAXNAMELEN + 1];
 				// 3. 把 path 和 name 拼接起来得到下一层文件路径，注意结尾的 '\0'
 				// 提示：我们没有实现 strcat 工具函数，你可以用 strcpy 实现拼接
-				strcpy(path, curpath);
-				curpath[len] = '/';
-				strcpy(f->f_name, curpath + len + 1);
+				strcpy(curpath, path);
+				//curpath[len] = '/';
+				strcpy(curpath + len, f->f_name);
 				int file_len = strlen(f->f_name);
-				curpath[len + 1 + file_len] = '\0';
+				curpath[len + file_len] = '/';
+				curpath[len + file_len + 1] = '\0';
 				// 4. 递归调用 traverse_file 函数
 				traverse_file(curpath, f, name, res);
 			}
@@ -53,10 +54,12 @@ int find_files(const char *path, const char *name, struct Find_res *res) {
         struct File *file;
         // 用 walk_path 来找到 path 对应的文件夹
         // Lab5-Exam: Your code here. (1/2)
-		walk_path(path, &file, NULL, NULL);
+		walk_path(path, &file, &file, NULL);
         // 在 path 对应的文件夹下面遍历，找到所有名字为 name 的文件，你可以调用下面的参考函数 traverse_file
         // Lab5-Exam: Your code here. (2/2)
 		traverse_file(path, file, name, res);
+
+		return 0;
 }
 
 // Overview:
