9843642bae45c9527ea550b03c9c3df2ac19a6c3
吴思远 <23371325@buaa.edu.cn>
2025-06-13 14:12:58 +0000

    debug

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -560,7 +560,8 @@ int sys_read_dev(u_int va, u_int pa, u_int len) {
 // for lab6-shell to change directory
 int sys_chdir(const char *path) {
 	// strcpy(curenv->env_cwd, path);
-	strcpy("omg", path);
+	printf("can you see me?\n");
+	strcpy(curenv->env_cwd, "omg");
     return 0;
 }
 
