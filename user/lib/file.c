d7c365db92341a386d90264214fc65e013e4e4f3
吴思远 <23371325@buaa.edu.cn>
2025-06-13 11:40:04 +0000

    pre 7th

--- user/lib/file.c
+++ user/lib/file.c
@@ -36,7 +36,9 @@ int open(const char *path, int mode) {
 
 	// Step 2: Prepare the 'fd' using 'fsipc_open' in fsipc.c.
 	/* Exercise 5.9: Your code here. (2/5) */
-	try(fsipc_open(path, mode, fd));
+	char abspath[MAXPATHLEN];
+	rel2abs(abspath, path);
+	try(fsipc_open(abspath, mode, fd));
 
 	// Step 3: Set 'va' to the address of the page where the 'fd''s data is cached, using
 	// 'fd2data'. Set 'size' and 'fileid' correctly with the value in 'fd' as a 'Filefd'.
