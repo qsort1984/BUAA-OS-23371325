66d10d5d80748dfd4ccb930656f43415eda2d7c2
吴思远 <23371325@buaa.edu.cn>
2025-06-14 12:04:53 +0000

    debug

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -5,9 +5,111 @@
 #include <printk.h>
 #include <sched.h>
 #include <syscall.h>
-#include <env_var.h>
+// #include <env_var.h>
 
 extern struct Env *curenv;
+
+#include <printf.h>
+#include <string.h>
+
+#define MAX_VARS 128
+#define MAX_NAME_LEN 16
+#define MAX_VAL_LEN 16
+
+struct EnvVar {
+    char name[MAX_NAME_LEN + 1];
+    char value[MAX_VAL_LEN + 1];
+    int shell_id; // 0 -> 全局环境变量 others -> 局部环境变量
+    int readonly;  // 是否是只读变量
+    int in_use;    // 是否占用
+};
+
+struct EnvVar env_vars[MAX_VARS];
+static int vars_num = 0;
+static int idNow = 1;
+
+int alloc_shell_id() {
+	return idNow++;
+}
+
+int declare_env_var(char *name, char *value, int shell_id, int readonly) {
+	int i;
+	for (i = 0; i < vars_num; i++) {
+		if (!env_vars[i].in_use) {
+			continue;
+		}
+		if (env_vars[i].shell_id == shell_id) {
+			if (strcmp(env_vars[i].name, name) == 0) {
+				if (env_vars[i].readonly) {
+					return -1;
+				} else {
+					strcpy(env_vars[i].value, value);
+					env_vars[i].readonly = readonly;
+					return 0;
+				}
+			}
+		}
+	}
+	// 未找到环境变量则需要重新声明
+	strcpy(env_vars[vars_num].name, name);
+	strcpy(env_vars[vars_num].value, value);
+	env_vars[vars_num].shell_id = shell_id;
+	env_vars[vars_num].readonly = readonly;
+	env_vars[vars_num].in_use = 1;
+	vars_num++;
+	return 0;
+}
+
+int unset_env_var(char *name, int shell_id) {
+	int i;
+	for (i = 0; i < vars_num; i++) {
+		if (!env_vars[i].in_use) {
+			continue;
+		}
+		if (env_vars[i].shell_id != shell_id && env_vars[i].shell_id != 0) {
+			continue;
+		}
+		if (strcmp(env_vars[i].name, name) == 0) {
+			if (env_vars[i].readonly == 1) {
+				return -1;
+			}
+			env_vars[i].in_use = 0;
+			return 0;
+		}
+	} 
+	return -1;
+}
+
+char *get_env_var(char *name, int shell_id) {
+	int i;
+	for (i = 0; i < vars_num; i++) {
+		if (!env_vars[i].in_use) {
+			continue;
+		}
+		if (env_vars[i].shell_id != shell_id && env_vars[i].shell_id != 0) {
+			continue;
+		}
+		if (strcmp(name, env_vars[i].name) == 0) {
+			return env_vars[i].value;
+		}
+	}
+
+	return name;
+}
+
+int print_vars(int shell_id) {
+	for (i = 0; i < vars_num; i++) {
+		if (!env_vars[i].in_use) {
+			continue;
+		}
+		if (env_vars[i].shell_id != shell_id && env_vars[i].shell_id != 0) {
+			continue;
+		}
+		printf("%s=%s\n", env_vars[i].name, env_vars[i].value);
+	}
+
+	return 0;
+}
 
 /* Overview:
  * 	This function is used to print a character on screen.
