35dce417aa3c4035732c851cf405f32f93f84c85
吴思远 <23371325@buaa.edu.cn>
2025-06-15 09:17:30 +0000

    debug

--- user/sh.c
+++ user/sh.c
@@ -456,23 +456,23 @@ void readline(char *buf, u_int n) {
 			// 	printf("\b");
 			// }
 			if (pointer != 0) {
-				for (int j = pointer - 1; j < max - 1; j++) {
-					buf[j] = buf[j + 1];
-				}
-				max--;
-				pointer--;
+				// for (int j = pointer - 1; j < max - 1; j++) {
+				// 	buf[j] = buf[j + 1];
+				// }
+				// max--;
+				// pointer--;
                 printf("\b"); 
 
 				// 重写后续字符覆盖原字符
-				for (int j = pointer; j < max; j++) {
-					printf("%c", buf[j]);
-				}
+				// for (int j = pointer; j < max; j++) {
+				// 	printf("%c", buf[j]);
+				// }
 				printf(" ");
 
 				// 光标右移回去
-				for (int j = max; j > pointer; j--) {
-					printf("\b");
-				}
+				// for (int j = max; j > pointer; j--) {
+				// 	printf("\b");
+				// }
 				printf("\b");
 				i -= 2;
 			} else {
