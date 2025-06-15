50939003122e5ab8271521f4330114060660283c
吴思远 <23371325@buaa.edu.cn>
2025-06-15 13:23:13 +0000

    debug

--- user/sh.c
+++ user/sh.c
@@ -517,10 +517,10 @@ void readline(char *buf, u_int n) {
 					current_index = (current_index + 1) % HISTORY_SIZE;
 					strcpy(buf, history_buf[current_index]);
 					printf("%s", buf);
-					for (int j = max; j < strlen(buf); j++) {
+					for (int j = strlen(buf); j < max; j++) {
 						printf(" ");
 					}
-					for (int j = max; j < strlen(buf); j++) {
+					for (int j = strlen(buf); j < max; j++) {
 						printf("\b");
 					}
 					i = max = pointer = strlen(buf);
