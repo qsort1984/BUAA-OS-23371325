befacc89cf7bc81bad5b213d77ffecdf4cb57bf4
吴思远 <23371325@buaa.edu.cn>
2025-06-16 05:18:17 +0000

    debug

--- user/sh.c
+++ user/sh.c
@@ -143,6 +143,7 @@ int run_command_and_capture_output(const char *cmd, char *output) {
 		} else {
 			output[0] = '\0';
 		}
+		printf("here is %s and is here\n", output);
 		close(p[0]);
 		wait(r);
 	}
