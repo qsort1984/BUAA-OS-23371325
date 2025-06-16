c7d5391965ef666b54ec508576b60e8257f314c0
吴思远 <23371325@buaa.edu.cn>
2025-06-16 05:53:09 +0000

    debug

--- user/sh.c
+++ user/sh.c
@@ -140,6 +140,12 @@ int run_command_and_capture_output(const char *cmd, char *output) {
 		close(p[1]);  // 关闭写端
 		int n = read(p[0], output, MAX_ARGV_LEN - 1);
 		if (n >= 0) {
+			for (int i < 0; i < n; i++) {
+				if (output[i] == '\n' || output[i] == '\r') {
+					output[i] = '\0';
+					break;
+				}
+			}
 			output[n] = '\0';
 		} else {
 			output[0] = '\0';
