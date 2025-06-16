8edd5e1a2650e880ae3c12b618960a9f6e8e3a37
吴思远 <23371325@buaa.edu.cn>
2025-06-16 08:32:04 +0000

    ???

--- user/sh.c
+++ user/sh.c
@@ -547,7 +547,7 @@ int runcmd(char *s) {
 	close_all();
 	if (child >= 0) {
 		// if (child_tag == 1) {
-			ipc_send(syscall_get_parent_id(), res, NULL, 0);
+		// 	ipc_send(syscall_get_parent_id(), res, NULL, 0);
 		// }
 		wait(child);
 	} else {
